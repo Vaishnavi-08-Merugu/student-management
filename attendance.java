99dd5f880287c4d907196b8c92bc94b780e1bd90
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-12-01 11:20:15 +0530

    Update attendance.java

--- attendance.java
+++ attendance.java
@@ -1,5 +1,5 @@
 public class attendance {
     private int days;
-    public Attendance(int d){ this.days=d; }
+    public attendance(int d){ this.days=d; }
     public int getDays(){ return days; }
 }
